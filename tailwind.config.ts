4599de876a2369ba155bd4344c74541ea477f6e2
WaruiAlfred <alfredwarui17@gmail.com>
2025-08-20 16:48:24 +0300

    Add color definitions for dark, red, and primary in Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,20 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          DEFAULT: "#0f0e0c",
+        },
+        red: {
+          DEFAULT: "##FF0000",
+        },
+        primary: {
+          DEFAULT: "#D32C1C",
+          light: "#D0A0A0",
+        },
+      },
+    },
   },
   plugins: [],
 };
