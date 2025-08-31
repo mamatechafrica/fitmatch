0541cba9afafc8d19dcdb1a651d25d0e1d3b62d1
WaruiAlfred <alfredwarui17@gmail.com>
2025-08-31 04:05:18 +0300

    feat: add Firebase configuration and update .gitignore for local env files

--- config/firebase.js
+++ config/firebase.js
@@ -5,15 +5,14 @@ import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDEsEFREav56tS7tOQq9V9fOo6Pg8zAnqg",
-  authDomain: "fit-match-6930a.firebaseapp.com",
-  databaseURL:
-    "https://fit-match-6930a-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "fit-match-6930a",
-  storageBucket: "fit-match-6930a.firebasestorage.app",
-  messagingSenderId: "925031854161",
-  appId: "1:925031854161:web:97d27bc6fd03b2c9c88870",
-  measurementId: "G-B964WWCCKT",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
